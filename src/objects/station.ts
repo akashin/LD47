39789c55471e24076b0fee8187b32234ef33d990
Alexander Novikov <sasha.v.novikov@gmail.com>
2020-10-03 21:53:00 +0100

    Add unique index to stations

--- src/objects/station.ts
+++ src/objects/station.ts
@@ -1,13 +1,16 @@
 import { CONST } from "../const";
 
 export class Station extends Phaser.GameObjects.Sprite {
+  static station_count: integer;
   column: integer;
   row: integer;
+  index: integer;
 
   constructor(scene, params) {
       super(scene, params.x, params.y, "station_tile");
       this.column = params.column;
       this.row = params.row;
+      this.index = ++Station.station_count;
 
       this.setOrigin(0, 0);
       this.setDisplaySize(CONST.tileSize, CONST.tileSize);
