ce5bc6c183b72e21a64cb0054157c6a8ffe2b1fd
akashin <kashin.andrej@gmail.com>
2020-10-03 16:17:16 +0100

    Small const refactoring

--- src/scenes/main_scene.ts
+++ src/scenes/main_scene.ts
@@ -2,7 +2,7 @@ import { CONST } from "../const";
 
 export class MainScene extends Phaser.Scene {
   private helloWorldText: Phaser.GameObjects.Text;
-  private msSinceLastTick: integer;
+  private msSinceLastTick: number;
   private tickCounter: integer;
 
   constructor() {
@@ -17,32 +17,31 @@ export class MainScene extends Phaser.Scene {
 
   // Initializes game state.
   init(): void {
+    this.msSinceLastTick = 0;
+    this.tickCounter = 0;
   }
 
   // Creates game objects.
   create(): void {
     var gameWidth = this.game.config.width as number;
     var gameHeight = this.game.config.height as number;
-    this.msSinceLastTick = 0;
-    this.tickCounter = 0;
     this.helloWorldText = this.add.text(
       gameWidth / 2 - 50, gameHeight / 2 - 20, "Hello, world!"
     )
-
   }
 
   // Called periodically to update game state.
   update(time: number, delta: number): void {
     this.msSinceLastTick += delta;
-    while (this.msSinceLastTick >= CONST['tickLen']) {
-      this.tickCounter += 1;
-      this.msSinceLastTick -= CONST['tickLen'];
+    while (this.msSinceLastTick >= CONST.tickDelta) {
+      this.msSinceLastTick -= CONST.tickDelta;
       this.updateStep();
     }
   }
 
   // Called every N ticks to update game state.
   updateStep(): void {
+    this.tickCounter += 1;
     this.helloWorldText.setText("Hello, wor!" + String(this.tickCounter));
   }
 }
