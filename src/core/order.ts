47fda310f71f6ec2489d91f4d56f76721f390bd4
Alexander Novikov <anovikov@google.com>
2020-10-03 17:53:58 +0100

    nits: add order status, increase order source scale

--- src/core/order.ts
+++ src/core/order.ts
@@ -1,8 +1,16 @@
+
+export enum OrderStatus {
+    offered,
+    taken,
+}
+
+
 export class Order {
     public startPosX: integer;
     public startPosY: integer;
     public endPosX: integer;
     public endPosY: integer;
+    public status: OrderStatus;
 
     // Creates Order objects.
     constructor(mapWidth: integer, mapHeight: integer) {
@@ -10,5 +18,6 @@ export class Order {
         this.startPosY = Math.floor(Math.random() * (mapHeight + 1));
         this.endPosX = Math.floor(Math.random() * (mapWidth + 1));
         this.endPosY = Math.floor(Math.random() * (mapHeight + 1));
+        this.status = OrderStatus.offered;
     }
   }
