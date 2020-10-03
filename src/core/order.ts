2747614e37e7bb873f2db48dcec582158f2835a6
Alexander Novikov <anovikov@google.com>
2020-10-03 21:36:05 +0100

    small refactoring of order manager

--- src/core/order.ts
+++ src/core/order.ts
@@ -24,7 +24,7 @@ export class Order {
 }
 
 export class OrderManager {
-    private stationLocations: Array<Array<integer>>;
+    private stations: Array<Station>;
     private openOrders: Array<Order>;
     private stationSourceOrder: Map<integer, Order>; // For each station shows an open order starting in it (if exist).
     private stationSinkOrders: Array<Array<Order>>; // For each station lists open orders ending in this station.
@@ -35,14 +35,10 @@ export class OrderManager {
     constructor(scene: Phaser.Scene, stations: Array<Station>) {
         this.scene = scene;
 
-        this.stationLocations = [];
-        for (let i = 0; i < stations.length; ++i) {
-            let station = [stations[i].column, stations[i].row];
-            this.stationLocations.push(station);
-        }
+        this.stations = stations;
 
         this.stationSinkOrders = [];
-        for (let i = 0; i < this.stationLocations.length; ++i) {
+        for (let i = 0; i < this.stations.length; ++i) {
             this.stationSinkOrders[i] = [];
         }
         this.openOrders = [];
@@ -51,8 +47,8 @@ export class OrderManager {
     }
 
     addOrder(): void {
-        if (this.openOrders.length < this.stationLocations.length) {
-            let numStations = this.stationLocations.length;
+        let numStations = this.stations.length;
+        if (this.openOrders.length < numStations) {
 
             let beginStation = randomInt(numStations);
             // Make sure source statation is not already used by some other order.
@@ -80,10 +76,8 @@ export class OrderManager {
 
     renderStationOrders(station: integer): void {
         // TODO: remove old sprites.
-        let loc = this.stationLocations[station];
-        let locX = loc[0] * CONST.tileSize;
-        let locY = loc[1] * CONST.tileSize;
-        console.log(station, this.stationSourceOrder, station in this.stationSourceOrder)
+        let locX = this.stations[station].column * CONST.tileSize;
+        let locY = this.stations[station].row * CONST.tileSize;
         if (station in this.stationSourceOrder) {
             let order = this.stationSourceOrder[station];
             var orderSource = new Phaser.GameObjects.Image(this.scene, locX, locY, 'orderSource');
