76855dcbb348c3293d892c33fa2f0392d68adb86
Alexander Novikov <sasha.v.novikov@gmail.com>
2020-10-03 16:04:02 +0100

    Add ticks, add constants

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -1,5 +1,9 @@
+import { CONST } from "../const";
+
 export class MainScene extends Phaser.Scene {
   private helloWorldText: Phaser.GameObjects.Text;
+  private msSinceLastTick: integer;
+  private tickCounter: integer;
 
   constructor() {
     super({
@@ -19,12 +23,26 @@ export class MainScene extends Phaser.Scene {
   create(): void {
     var gameWidth = this.game.config.width as number;
     var gameHeight = this.game.config.height as number;
+    this.msSinceLastTick = 0;
+    this.tickCounter = 0;
     this.helloWorldText = this.add.text(
       gameWidth / 2 - 50, gameHeight / 2 - 20, "Hello, world!"
     )
+
   }
 
   // Called periodically to update game state.
   update(time: number, delta: number): void {
+    this.msSinceLastTick += delta;
+    while (this.msSinceLastTick >= CONST['tickLen']) {
+      this.tickCounter += 1;
+      this.msSinceLastTick -= CONST['tickLen'];
+      this.updateStep();
+    }
+  }
+
+  // Called every N ticks to update game state.
+  updateStep(): void {
+    this.helloWorldText.setText("Hello, wor!" + String(this.tickCounter));
   }
 }
