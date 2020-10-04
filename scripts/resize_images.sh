49dbdd7074c313e36485defae0f9d2b35599530a
akashin <kashin.andrej@gmail.com>
2020-10-04 20:34:57 +0100

    Add new spritesheet

--- scripts/resize_images.sh
+++ scripts/resize_images.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 cd assets
-convert original/station.png -resize 128x128 pack/station.png
-convert original/rails_top_bottom.png -resize 64x64 pack/rails_top_bottom.png
-convert original/rails_top_bottom.png -resize 64x64 -rotate 90 pack/rails_left_right.png
-convert original/rails_top_right.png -resize 64x64 pack/rails_top_right.png
-convert original/rails_top_right.png -resize 64x64 -rotate 90 pack/rails_right_bottom.png
-convert original/rails_top_right.png -resize 64x64 -rotate 180 pack/rails_bottom_left.png
-convert original/rails_top_right.png -resize 64x64 -rotate 270 pack/rails_left_top.png
-convert original/blank.png -resize 64x64 pack/blank.png
-convert original/blank.png -resize 64x64 pack/blank.png
+convert original/station.png -resize 32x32 pack/station.png
+convert original/factory.png -resize 32x32 pack/factory.png
+convert original/factory_no_base.png -resize 32x32 pack/factory_no_base.png
+convert original/rails_top_bottom.png -resize 32x32 pack/rails_top_bottom.png
+convert original/rails_top_bottom.png -resize 32x32 -rotate 90 pack/rails_left_right.png
+convert original/rails_top_right.png -resize 32x32 pack/rails_top_right.png
+convert original/rails_top_right.png -resize 32x32 -rotate 90 pack/rails_right_bottom.png
+convert original/rails_top_right.png -resize 32x32 -rotate 180 pack/rails_bottom_left.png
+convert original/rails_top_right.png -resize 32x32 -rotate 270 pack/rails_left_top.png
+convert original/blank.png -resize 32x32 pack/blank.png
+convert original/blank.png -resize 32x32 pack/blank_1.png
+convert original/blank.png -resize 32x32 pack/blank_2.png
+convert original/blank.png -resize 32x32 pack/blank_3.png
+convert original/blank.png -resize 32x32 pack/blank_4.png
